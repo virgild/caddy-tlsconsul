75f829067eb3e609330103a67cf6ee2ca5871b36
Peter Teich <peter.teich@gmail.com>
2020-06-07 00:28:25 +0200

    Implement UnmarshalCaddyfile to support Caddyfile setting, Split out file and optimize config

--- crypto.go
+++ crypto.go
@@ -9,86 +9,86 @@ import (
 	"io"
 )
 
-func (cs *ConsulStorage) encrypt(bytes []byte) ([]byte, error) {
+func (s *Storage) encrypt(bytes []byte) ([]byte, error) {
 	// No key? No encrypt
-	if len(cs.aesKey) == 0 {
+	if len(s.config.AESKey) == 0 {
 		return bytes, nil
 	}
 
-	c, err := aes.NewCipher(cs.aesKey)
+	c, err := aes.NewCipher(s.config.AESKey)
 	if err != nil {
-		return nil, fmt.Errorf("unable to create AES cipher: %v", err)
+		return nil, fmt.Errorf("unable to create AES cipher: %w", err)
 	}
 
 	gcm, err := cipher.NewGCM(c)
 	if err != nil {
-		return nil, fmt.Errorf("unable to create GCM cipher: %v", err)
+		return nil, fmt.Errorf("unable to create GCM cipher: %w", err)
 	}
 
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
-		return nil, fmt.Errorf("unable to generate nonce: %v", err)
+		return nil, fmt.Errorf("unable to generate nonce: %w", err)
 	}
 
 	return gcm.Seal(nonce, nonce, bytes, nil), nil
 }
 
-func (cs *ConsulStorage) EncryptStorageData(data *StorageData) ([]byte, error) {
+func (s *Storage) EncryptStorageData(data *StorageData) ([]byte, error) {
 	// JSON marshal, then encrypt if key is there
 	bytes, err := json.Marshal(data)
 	if err != nil {
-		return nil, fmt.Errorf("unable to marshal: %v", err)
+		return nil, fmt.Errorf("unable to marshal: %w", err)
 	}
 
 	// Prefix with simple prefix and then encrypt
-	bytes = append([]byte(cs.valuePrefix), bytes...)
-	return cs.encrypt(bytes)
+	bytes = append([]byte(s.config.ValuePrefix), bytes...)
+	return s.encrypt(bytes)
 }
 
-func (cs *ConsulStorage) decrypt(bytes []byte) ([]byte, error) {
+func (s *Storage) decrypt(bytes []byte) ([]byte, error) {
 	// No key? No decrypt
-	if len(cs.aesKey) == 0 {
+	if len(s.config.AESKey) == 0 {
 		return bytes, nil
 	}
 	if len(bytes) < aes.BlockSize {
 		return nil, fmt.Errorf("invalid contents")
 	}
 
-	block, err := aes.NewCipher(cs.aesKey)
+	block, err := aes.NewCipher(s.config.AESKey)
 	if err != nil {
-		return nil, fmt.Errorf("unable to create AES cipher: %v", err)
+		return nil, fmt.Errorf("unable to create AES cipher: %w", err)
 	}
 
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, fmt.Errorf("unable to create GCM cipher: %v", err)
+		return nil, fmt.Errorf("unable to create GCM cipher: %w", err)
 	}
 
 	out, err := gcm.Open(nil, bytes[:gcm.NonceSize()], bytes[gcm.NonceSize():], nil)
 	if err != nil {
-		return nil, fmt.Errorf("decryption failure: %v", err)
+		return nil, fmt.Errorf("decryption failure: %w", err)
 	}
 
 	return out, nil
 }
 
-func (cs *ConsulStorage) DecryptStorageData(bytes []byte) (*StorageData, error) {
+func (s *Storage) DecryptStorageData(bytes []byte) (*StorageData, error) {
 	// We have to decrypt if there is an AES key and then JSON unmarshal
-	bytes, err := cs.decrypt(bytes)
+	bytes, err := s.decrypt(bytes)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to decrypt data: %w", err)
 	}
 
 	// Simple sanity check of the beginning of the byte array just to check
-	if len(bytes) < len(cs.valuePrefix) || string(bytes[:len(cs.valuePrefix)]) != cs.valuePrefix {
+	if len(bytes) < len(s.config.ValuePrefix) || string(bytes[:len(s.config.ValuePrefix)]) != s.config.ValuePrefix {
 		return nil, fmt.Errorf("invalid data format")
 	}
 
 	// Now just json unmarshal
 	data := &StorageData{}
-	if err := json.Unmarshal(bytes[len(cs.valuePrefix):], data); err != nil {
-		return nil, fmt.Errorf("unable to unmarshal result: %v", err)
+	if err := json.Unmarshal(bytes[len(s.config.ValuePrefix):], data); err != nil {
+		return nil, fmt.Errorf("unable to unmarshal result: %w", err)
 	}
 	return data, nil
 }
--- module.go
+++ module.go
@@ -1,35 +1,102 @@
 package storageconsul
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/certmagic"
 )
 
-type TLSConsul struct {
-	storage *ConsulStorage
-}
-
 func init() {
-	caddy.RegisterModule(&TLSConsul{})
+	caddy.RegisterModule(Storage{})
 }
 
-func (TLSConsul) CaddyModule() caddy.ModuleInfo {
+func (Storage) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "caddy.storage.tlsconsul",
-		New: func() caddy.Module { return new(TLSConsul) },
+		ID: "caddy.storage.consul",
+		New: func() caddy.Module {
+			return New()
+		},
 	}
 }
 
 // Provision is called by Caddy to prepare the module
-func (tlsc *TLSConsul) Provision(ctx caddy.Context) error {
-	consulStorage, err := NewConsulStorage()
-	if err != nil {
-		return err
-	}
-	tlsc.storage = consulStorage
-	return nil
+func (s *Storage) Provision(ctx caddy.Context) error {
+	s.logger = ctx.Logger(s).Sugar()
+	s.logger.Infof("TLS storage is using Consul at %s", s.config.ConsulAddr)
+	return s.createConsulClient()
 }
 
-func (tlsc *TLSConsul) CertMagicStorage() (certmagic.Storage, error) {
-	return tlsc.storage, nil
+func (s *Storage) CertMagicStorage() (certmagic.Storage, error) {
+	return s, nil
+}
+
+// UnmarshalCaddyfile parses plugin settings from Caddyfile
+// storage consul {
+//     address      "127.0.0.1:8500"
+//     token        "consul-access-token"
+//     timeout      10
+//     prefix       "caddytls"
+//     value_prefix "myprefix"
+//     aes_key      "consultls-1234567890-caddytls-32"
+//     tls_enabled  "false"
+//     tls_insecure "true"
+// }
+func (s *Storage) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		args := d.RemainingArgs()
+		if len(args) > 1 {
+			switch args[0] {
+			case "address":
+				if args[1] != "" {
+					parsedAddress, err := caddy.ParseNetworkAddress(args[1])
+					if err == nil {
+						WithConsulAddr(parsedAddress.JoinHostPort(0))(&s.config)
+					}
+				}
+			case "token":
+				if args[1] != "" {
+					WithConsulToken(args[1])(&s.config)
+				}
+			case "timeout":
+				if args[1] != "" {
+					timeParse, err := strconv.Atoi(args[1])
+					if err == nil {
+						WithTimeout(time.Duration(timeParse) * time.Second)(&s.config)
+					}
+				}
+			case "prefix":
+				if args[1] != "" {
+					WithPrefix(args[1])(&s.config)
+				}
+			case "value_prefix":
+				if args[1] != "" {
+					WithValuePrefix(args[1])(&s.config)
+				}
+			case "aes_key":
+				if args[1] != "" {
+					WithAESKey(args[1])
+				}
+			case "tls_enabled":
+				if args[1] != "" {
+					tlsParse, err := strconv.ParseBool(args[1])
+					if err == nil {
+						WithConsulTls(tlsParse)(&s.config)
+					}
+				}
+			case "tls_insecure":
+				if args[1] != "" {
+					tlsInsecureParse, err := strconv.ParseBool(args[1])
+					if err == nil {
+						WithConsulTlsInsecure(tlsInsecureParse)
+					}
+				}
+			}
+		} else {
+			continue
+		}
+	}
+	return nil
 }
