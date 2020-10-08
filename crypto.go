9e9ac1538044a616a04ed45c66d1aa585fbb7fb1
Peter Teich <peter.teich@gmail.com>
2020-10-08 18:49:54 +0200

    Support for Caddyfile

--- crypto.go
+++ crypto.go
@@ -11,11 +11,11 @@ import (
 
 func (s *Storage) encrypt(bytes []byte) ([]byte, error) {
 	// No key? No encrypt
-	if len(s.config.AESKey) == 0 {
+	if len(s.AESKey) == 0 {
 		return bytes, nil
 	}
 
-	c, err := aes.NewCipher(s.config.AESKey)
+	c, err := aes.NewCipher(s.AESKey)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create AES cipher: %w", err)
 	}
@@ -42,20 +42,20 @@ func (s *Storage) EncryptStorageData(data *StorageData) ([]byte, error) {
 	}
 
 	// Prefix with simple prefix and then encrypt
-	bytes = append([]byte(s.config.ValuePrefix), bytes...)
+	bytes = append([]byte(s.ValuePrefix), bytes...)
 	return s.encrypt(bytes)
 }
 
 func (s *Storage) decrypt(bytes []byte) ([]byte, error) {
 	// No key? No decrypt
-	if len(s.config.AESKey) == 0 {
+	if len(s.AESKey) == 0 {
 		return bytes, nil
 	}
 	if len(bytes) < aes.BlockSize {
 		return nil, fmt.Errorf("invalid contents")
 	}
 
-	block, err := aes.NewCipher(s.config.AESKey)
+	block, err := aes.NewCipher(s.AESKey)
 	if err != nil {
 		return nil, fmt.Errorf("unable to create AES cipher: %w", err)
 	}
@@ -81,13 +81,13 @@ func (s *Storage) DecryptStorageData(bytes []byte) (*StorageData, error) {
 	}
 
 	// Simple sanity check of the beginning of the byte array just to check
-	if len(bytes) < len(s.config.ValuePrefix) || string(bytes[:len(s.config.ValuePrefix)]) != s.config.ValuePrefix {
+	if len(bytes) < len(s.ValuePrefix) || string(bytes[:len(s.ValuePrefix)]) != s.ValuePrefix {
 		return nil, fmt.Errorf("invalid data format")
 	}
 
 	// Now just json unmarshal
 	data := &StorageData{}
-	if err := json.Unmarshal(bytes[len(s.config.ValuePrefix):], data); err != nil {
+	if err := json.Unmarshal(bytes[len(s.ValuePrefix):], data); err != nil {
 		return nil, fmt.Errorf("unable to unmarshal result: %w", err)
 	}
 	return data, nil
